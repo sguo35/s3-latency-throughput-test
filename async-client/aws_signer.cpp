ec59cd705f9b0d3076ced2807d2f099582cccee2
Shiyuan Guo <shiyuan.guo@berkeley.edu>
2020-10-05 11:59:57 -0700

    fix char typing

--- async-client/aws_signer.cpp
+++ async-client/aws_signer.cpp
@@ -165,33 +165,33 @@ std::string calculate_signature(std::string secret_key, boost::posix_time::ptime
     std::string date_key_hmac_data = boost::posix_time::to_iso_string(time).substr(0, 8);
 
     // calculate DateKey
-    const char* date_key_key = date_key_hmac_key.c_str();
-    const char* date_key_text = date_key_hmac_data.c_str();
+    const unsigned char* date_key_key = date_key_hmac_key.c_str();
+    const unsigned char* date_key_text = date_key_hmac_data.c_str();
     char date_key_digest[HMAC_DIGEST_SIZE];
 
     hmac_sha256(date_key_key, date_key_hmac_key.length(), date_key_text, date_key_hmac_data.length(), date_key_digest);
     date_key(date_key_digest, HMAC_DIGEST_SIZE);
 
     // calculate DateRegionKey
-    const char* date_region_key_text = region.c_str();
+    const unsigned char* date_region_key_text = region.c_str();
     char date_region_key_digest[HMAC_DIGEST_SIZE];
 
     hmac_sha256(date_key_digest, HMAC_DIGEST_SIZE, date_region_key_text, region.length(), date_region_key_digest);
 
     // calculate DateRegionServiceKey
-    const char* date_region_service_key_text = service.c_str();
+    const unsigned char* date_region_service_key_text = service.c_str();
     char date_region_service_key_digest[HMAC_DIGEST_SIZE];
 
     hmac_sha256(date_region_key_digest, HMAC_DIGEST_SIZE, date_region_service_key_text, service.length(), date_region_service_key_digest);
 
     // calculate SigningKey
-    const char* signing_key_text = AWS_SIGNING_KEY_SUFFIX.c_str();
+    const unsigned char* signing_key_text = AWS_SIGNING_KEY_SUFFIX.c_str();
     char signing_key_digest[HMAC_DIGEST_SIZE];
 
     hmac_sha256(date_region_service_key_digest, HMAC_DIGEST_SIZE, signing_key_text, AWS_SIGNING_KEY_SUFFIX.length(), signing_key_digest);
 
     // final signature
-    const char* string_to_sign_text = string_to_sign.c_str();
+    const unsigned char* string_to_sign_text = string_to_sign.c_str();
     char signature_digest[HMAC_DIGEST_SIZE];
 
     hmac_sha256(signing_key_digest, HMAC_DIGEST_SIZE, string_to_sign_text, string_to_sign.length(), signature_digest);
